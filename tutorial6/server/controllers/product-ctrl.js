a9029a7c2d1cab6536ca220f8cfd7c974d558753
inna-i <iivashchuk@talend.com>
2021-08-28 13:23:22 +0300

    chore: improve code

--- tutorial6/server/controllers/product-ctrl.js
+++ tutorial6/server/controllers/product-ctrl.js
@@ -1,19 +1,24 @@
 const Product = require('../models/product-model');
 
+function sendResponseWithFailure(res, code, error) {
+	return res.status(code).json({
+		success: false,
+		error,
+	});
+}
+
+// CRUD operations
 const createProduct = (req, res) => {
 	const body = req.body;
 
 	if (!body) {
-		return res.status(400).json({
-			success: false,
-			error: 'You must provide a product',
-		});
+		return sendResponseWithFailure(res, 400, 'You must provide a product details');
 	}
 
 	const product = new Product(body);
 
 	if (!product) {
-		return res.status(400).json({ success: false, error: err });
+		return sendResponseWithFailure(res, 400, 'No product');
 	}
 
 	product
@@ -25,30 +30,42 @@ const createProduct = (req, res) => {
 				message: 'Product created',
 			});
 		})
-		.catch(error => {
-			return res.status(400).json({
-				error,
-				message: 'Product not created',
-			});
-		});
+		.catch(e => sendResponseWithFailure(res, 400, 'Product not created'));
 };
 
-const updateProduct = async (req, res) => {
+const getProductById = (req, res) => {
+	Product.findOne({ _id: req.params.id })
+		.then(product => {
+			if (!product) {
+				return sendResponseWithFailure(res, 404, 'Product not found');
+			}
+			return res.status(200).json({ success: true, data: product });
+		})
+		.catch(error => sendResponseWithFailure(res, 400, error));
+};
+
+const getProducts = (req, res) => {
+	Product.find({})
+		.then(products => {
+			if (!products.length) {
+				return sendResponseWithFailure(res, 404, 'Products not found');
+			}
+
+			return res.status(200).json({ success: true, data: products });
+		})
+		.catch(error => console.log(error));
+};
+
+const updateProduct = (req, res) => {
 	const body = req.body;
 
 	if (!body) {
-		return res.status(400).json({
-			success: false,
-			error: 'You must provide a body to update',
-		});
+		return sendResponseWithFailure(res, 400, 'You must provide a valid payload to update');
 	}
 
-	Product.findOne({ _id: req.params.id }, (err, product) => {
-		if (err) {
-			return res.status(404).json({
-				err,
-				message: 'Product not found!',
-			});
+	Product.findOne({ _id: req.params.id }, (error, product) => {
+		if (error) {
+			return sendResponseWithFailure(res, 400, 'Product is not created');
 		}
 		product.type = body.type;
 		product.price = body.price;
@@ -59,54 +76,25 @@ const updateProduct = async (req, res) => {
 				return res.status(200).json({
 					success: true,
 					id: product._id,
-					message: 'Product updated!',
+					message: 'Product updated',
 				});
 			})
-			.catch(error => {
-				return res.status(404).json({
-					error,
-					message: 'Product not updated!',
-				});
-			});
+			.catch(errorDetails => sendResponseWithFailure(res, 400, errorDetails));
 	});
 };
 
-const deleteProduct = async (req, res) => {
-	await Product.findOneAndDelete({ _id: req.params.id }, (err, product) => {
-		if (err) {
-			return res.status(400).json({ success: false, error: err });
+const deleteProduct = (req, res) => {
+	Product.findOneAndDelete({ _id: req.params.id }, (error, product) => {
+		if (error) {
+			return sendResponseWithFailure(res, 400, error);
 		}
 
 		if (!product) {
-			return res.status(404).json({ success: false, error: `Product not found` });
+			return sendResponseWithFailure(res, 404, 'Product not found');
 		}
 
 		return res.status(200).json({ success: true, data: product });
-	}).catch(err => console.log(err));
-};
-
-const getProductById = async (req, res) => {
-	await Product.findOne({ "_id": req.params.id}, (err, product) => {
-        console.log(req.params.id, ' type --------- ', typeof Number(req.params.id));
-        
-		if (err) {
-			return res.status(400).json({ success: false, error: err });
-		}
-
-		return res.status(200).json({ success: true, data: product });
-	}).catch(err => console.log(err));
-};
-
-const getProducts = async (req, res) => {
-	await Product.find({}, (err, products) => {
-		if (err) {
-			return res.status(400).json({ success: false, error: err });
-		}
-		if (!products.length) {
-			return res.status(404).json({ success: false, error: `Product not found` });
-		}
-		return res.status(200).json({ success: true, data: products });
-	}).catch(err => console.log(err));
+	}).catch(error => console.log(error));
 };
 
 module.exports = {
